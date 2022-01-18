0fb1430c9b693adbcb43fe9f6b74a79bc621e654
Jeff El'Ducati <geomondi09@gmail.com>
2022-01-18 08:54:16 +0000

    modified the keypair functionality, updated sendGif

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import React,{useEffect,useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+import idl from './idl.json';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+import {Buffer} from 'buffer'
+window.Buffer = Buffer;
+import kp from "./keypair.json"
+
+// SystemProgram is a reference to the Solana runtime!
+const { SystemProgram, Keypair } = web3;
+
+// Create a keypair for the account that will hold the GIF data.
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
+
+// Get our program's id from the IDL file.
+const programID = new PublicKey(idl.metadata.address);
+
+// Set our network to devnet.
+const network = clusterApiUrl('devnet');
+
+// Controls how we want to acknowledge when a transaction is "done".
+const opts = {
+  preflightCommitment: "processed"
+}
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -58,13 +83,28 @@ const [gifList, setGifList] = useState([]);
   }
   };
    
-   const sendGif = async () => {
-  if (inputValue.length > 0) {
-    console.log('Gif link:', inputValue);
-    setGifList([...gifList, inputValue]);
-    setInputValue('');
-  } else {
-    console.log('Empty input. Try again.');
+ const sendGif = async () => {
+  if (inputValue.length === 0) {
+    console.log("No gif link given!")
+    return
+  }
+  setInputValue('');
+  console.log('Gif link:', inputValue);
+  try {
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
+
+    await program.rpc.addGif(inputValue, {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
+    console.log("GIF successfully sent to program", inputValue)
+
+    await getGifList();
+  } catch (error) {
+    console.log("Error sending GIF:", error)
   }
 };
 
@@ -72,6 +112,35 @@ const [gifList, setGifList] = useState([]);
   const { value } = event.target;
   setInputValue(value);
 };
+
+const getProvider = () => {
+  const connection = new Connection(network, opts.preflightCommitment);
+  const provider = new Provider(
+    connection, window.solana, opts.preflightCommitment,
+  );
+	return provider;
+}
+
+const createGifAccount = async () => {
+  try {
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
+    console.log("ping")
+    await program.rpc.startStuffOff({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount]
+    });
+    console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+    await getGifList();
+
+  } catch(error) {
+    console.log("Error creating BaseAccount account:", error)
+  }
+}
 
   /*
    * We want to render this UI when the user hasn't connected
@@ -86,30 +155,49 @@ const [gifList, setGifList] = useState([]);
     </button>
   );
 
-  const renderConnectedContainer = () => (
-  <div className="connected-container">
-   {/* Go ahead and add this input and button to start */}
-    <form
-  onSubmit={(event) => {
-    event.preventDefault();
-    sendGif();
-  }}
->
-      <input type="text" 
-      placeholder="Enter gif link!" 
-      value={inputValue}
-      onChange={onInputChange}/>
-      <button type="submit" className="cta-button submit-gif-button">Submit</button>
-    </form>
-    <div className="gif-grid">
-      {gifList.map(gif => (
-        <div className="gif-item" key={gif}>
-          <img src={gif} alt={gif} />
+  const renderConnectedContainer = () => {
+// If we hit this, it means the program account hasn't been initialized.
+  if (gifList === null) {
+    return (
+      <div className="connected-container">
+        <button className="cta-button submit-gif-button" onClick={createGifAccount}>
+          Do One-Time Initialization For GIF Program Account
+        </button>
+      </div>
+    )
+  } 
+	// Otherwise, we're good! Account exists. User can submit GIFs.
+	else {
+    return(
+      <div className="connected-container">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            sendGif();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Enter gif link!"
+            value={inputValue}
+            onChange={onInputChange}
+          />
+          <button type="submit" className="cta-button submit-gif-button">
+            Submit
+          </button>
+        </form>
+        <div className="gif-grid">
+					{/* We use index as the key instead, also, the src is now item.gifLink */}
+          {gifList.map((item, index) => (
+            <div className="gif-item" key={index}>
+              <img src={item.gifLink} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </div>
-);
+      </div>
+    )
+  }
+}
 
   /*
    * When our component first mounts, let's check to see if we have a connected
@@ -123,14 +211,28 @@ const [gifList, setGifList] = useState([]);
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  const getGifList = async() => {
+  try {
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    
+    console.log("Got the account", account)
+    setGifList(account.gifList)
+
+  } catch (error) {
+    console.log("Error in getGifList: ", error)
+    setGifList(null);
+  }
+}
+
   useEffect(() => {
   if (walletAddress) {
     console.log('Fetching GIF list...');
     
     // Call Solana program here.
-
-    // Set state
-    setGifList(TEST_GIFS);
+    getGifList()
+       
   }
 }, [walletAddress]);
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import {defineConfig} from 'vite'
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
-export default {
+export default defineConfig({
   plugins: [reactRefresh()],
   server: {
     host: '0.0.0.0',
     hmr: {
       port: 443,
-    }
-  }
-}
+    },
+  },
+  define: {
+    global:{},
+  },
+})
